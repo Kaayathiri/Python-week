420198316b467964743cf68d89c32592f82920ed
Kaayathiri <50659294+Kaayathiri@users.noreply.github.com>
2019-05-24 10:01:05 +0100

    Add files via upload

---
+++ results.py
@@ -0,0 +1,6 @@
+physics=80
+chemistry=100
+math=100
+total=physics+chemistry+math
+print ("The result is")
+print (total)
---
+++ results2.py
@@ -0,0 +1,12 @@
+physics=80
+chemistry=100
+maths=100
+total=physics+chemistry+maths
+per=total*100/150
+print ("Physics:",physics)
+print ("Chem:",chemistry)
+print ("Maths:",maths)
+print ("---------------------")
+print ("Total Marks:",total)
+print ("Percentage:",per, "%")
+print ("---------------------")
